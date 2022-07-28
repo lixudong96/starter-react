7b499c0b2c38a0b7363afa2d50dc383f0ed3c192
SunLee <303001274@qq.com>
2022-07-28 23:53:57 +0800

    docs: delete annotates

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import 'antd/dist/antd.less'
 import 'uno.css'
@@ -9,9 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <BrowserRouter>
-    {/* <Routes>
-      <Route path="/" element={<App />}></Route>
-    </Routes> */}
     <App />
   </BrowserRouter>,
 )
