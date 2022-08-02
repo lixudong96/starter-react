083167698fcb2731eedbd232c14633744cb7c0fc
sunlee <303001274@qq.com>
2022-08-02 16:21:34 +0800

    feat(lib): add react-query

---
+++ src/lib/react-query.ts
@@ -0,0 +1,28 @@
+import type { AxiosError } from 'axios'
+import type { DefaultOptions, UseMutationOptions, UseQueryOptions } from '@tanstack/react-query'
+import { QueryClient } from '@tanstack/react-query'
+
+const queryConfig: DefaultOptions = {
+  queries: {
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false,
+    retry: false,
+  },
+}
+
+export const queryClient = new QueryClient({ defaultOptions: queryConfig })
+
+export type ExtractFnReturnType<FnType extends (...args: any) => any> = Awaited<
+  ReturnType<FnType>
+>
+
+export type QueryConfig<QueryFnType extends (...args: any) => any> = Omit<
+  UseQueryOptions<ExtractFnReturnType<QueryFnType>>,
+  'queryKey' | 'queryFn'
+>
+
+export type MutationConfig<MutationFnType extends (...args: any) => any> = UseMutationOptions<
+  ExtractFnReturnType<MutationFnType>,
+  AxiosError,
+  Parameters<MutationFnType>[0]
+>
