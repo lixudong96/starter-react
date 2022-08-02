7c80a06ff42f1ab66af5c5dfe6a5706590d01a69
sunlee <303001274@qq.com>
2022-08-02 12:04:33 +0800

    feat(routes): update demo

---
+++ src/features/demo/routes/Demo.tsx
@@ -0,0 +1,13 @@
+import { Link, Outlet } from 'react-router-dom'
+
+export const Demo = () => {
+  return (
+    <div>
+      <nav className="my-2">
+        <Link className="mr-5" to="/demo/recoil">Recoil</Link>
+        <Link to="/demo/router/333">router</Link>
+      </nav>
+      <Outlet />
+    </div>
+  )
+}
--- src/features/demo/routes/index.tsx
+++ src/features/demo/routes/index.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
+import { Demo } from './Demo'
 import { Recoid } from './Recoil'
 import { Router } from './Router'
 
 export const DemoRoutes = () => {
   return (
     <Routes>
-      <Route path="recoil" element={<Recoid />} />
-      <Route path="router/:id" element={<Router />} />
+      <Route path="/" element={<Demo />}>
+        <Route path="recoil" element={<Recoid />} />
+        <Route path="router/:id" element={<Router />} />
+      </Route>
     </Routes>
   )
 }
